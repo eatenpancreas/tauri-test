3123bbfdd9f07483c4d59e68fd02a8df190450d9
Pascal d'Achard <0309597@student.rocvantwente.nl>
2024-02-25 12:44:03 +0100

    Error: waiting for frontend dev server to start

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    let message = $state("I love Tauri + Svelte")
+    
+    function click() {
+        message += " + Svelte";
+    }
+</script>
+
+<button class="border-black border bg-gray-300 p-2" on:click={click}>Press me.</button>
+
+<h1 class="text-2xl font-bold">Welcome to SvelteKit!</h1>
+<h2 class="text-blue-800">{message}</h2>
+<p>Visit <a class="text-blue-300 border-b border-blue-300" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
